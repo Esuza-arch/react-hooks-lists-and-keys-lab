39c32e5acf16d0b080f5a080b68cb0b2965d0365
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-19 17:04:05 +0300

    Finish lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,8 +5,12 @@ function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <h2>My Projects</h2>
+    <div id="project-list">
+      {projects.map((project) => (
+        <ProjectItem key={project.id} {...project} />
+      ))}
+    </div>
     </div>
   );
 }
